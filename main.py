0a39327d80523e7e20f26379f65dcae0614e6ea6
Neo Lee <neo.lee.god@gmail.com>
2018-10-01 00:06:38 +0800

    update hcho

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 """
 # MicroPython ST7735 TFT display driver example usage
 # Used Pin 13,16,17,18,19,23,26 for display
-# Used Pin 22 for dht
+# Used Pin 5 for dht
 # USed Pin 4 for touch1
 # Used Pin 27,32,33 for LED
+# Used Pin  0, 22 for Uart
 """
 from machine import Pin, SPI, TouchPad
 from tft import TFT_GREEN
 from font import terminalfont
 import dht
 import time
+import ubinascii
+import urequests as requests
+from machine import UART
 
+
+#define UART
+uart = UART(2, baudrate=9600, bits=8, parity=None, stop=1,rx=0, tx=22, timeout=1000)
+
+#define TFT
 # DC       - RS/DC data/command flag
 # CS       - Chip Select, enable communication
 # RST/RES  - Reset
@@ -36,33 +45,72 @@ B = Pin(33, Pin.OUT, None)
 
 
 def get_room_temp():
-    d = dht.DHT11(Pin(22))
+    d = dht.DHT11(Pin(5))
     d.measure()
     return  d.temperature(),d.humidity()
+
+def get_hcho():
+    a = 'ff0178410000000046'
+    b = 'ff0186000000000079'
+    a_bytes = ubinascii.unhexlify(a)  # 传感器改问答式
+    b_bytes = ubinascii.unhexlify(b)  # 询问数值
+    print(a_bytes)
+    print(b_bytes)
+    uart.write(b'\xff\x01xA\x00\x00\x00\x00F')
+    # utime.sleep_ms(1000)
+    uart.write(b'\xff\x01\x86\x00\x00\x00\x00\x00y')
+    if uart.any():
+        q = uart.read(9)
+        #q= uart.readall()
+        print('q')
+        print(q)#获取9个数值
+        qq = ubinascii.hexlify(q)
+        print('qq')#16转str
+        print (qq)
+    #数值切片
+        g = qq[4:6]
+        d = qq[6:8]
+    #16转10
+        gg = int(g,16)
+        dd = int(d,16)
+        print (g)
+        print (d)
+        print (gg)
+        print (dd)
+    #计算最终值
+        r = gg*256 + dd
+
+        print ('result =' + str(r))
+        return r
+
 
 def display():
     # init TFT
     tft.init()
     # start using the driver
     tft.clear(tft.rgbcolor(255, 255, 255))
-    r = get_room_temp()
-    temperature = 'TEMP:' + str(r[0])
-    humidity = 'Hum:' + str(r[1])
+    r1 = get_room_temp()
+    r2 = get_hcho()
+    temperature = 'TEMP:' + str(r1[0])
+    humidity = 'Hum:' + str(r1[1])
+    hcho = 'HCHO:' + str(r2)
     tft.text(5, 50, temperature, terminalfont, tft.rgbcolor(0, 255, 0), 2)
     tft.text(5, 70, humidity, terminalfont, tft.rgbcolor(255, 255, 0), 2)
-
+    tft.text(5, 90, hcho, terminalfont, tft.rgbcolor(255, 255, 0), 2)
 
 def main():
-    #display() & shut rgbled
-    R.value(1)
-    G.value(1)
-    B.value(1)
     while True:
-        if touch1.read() < 50 :
-            print(touch1.read())
-            display()
-        else:
-            print(touch1.read())
+        display()
+        time.sleep(5)
+    # R.value(1)
+    # G.value(1)
+    # B.value(1)
+    # while True:
+    #     if touch1.read() < 50 :
+    #         print(touch1.read())
+    #         display()
+    #     else:
+    #         print(touch1.read())
 
 
         #upload_wx()
