9bdebba5ce59b3792d4bf3b77bf2877df92c9db3
Neo Lee <neo.lee.god@gmail.com>
2018-10-16 00:34:40 +0800

    gps branch first commit

--- boot.py
+++ boot.py
@@ -5,18 +5,19 @@ import gc
 #import webrepl
 import network
 import time
-import ntptime
+#import ntptime
 import machine
 import utime
 
 
 #set wlan
-ap = network.WLAN(network.AP_IF)
-ap.active(True)
+#ap = network.WLAN(network.AP_IF)
+#ap.active(True)
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
+#time.sleep(2)
 wlan.connect('XiaoPiDan', 'asdfzxcv123')
-#time.sleep(3)
+time.sleep(3)
 if wlan.isconnected() == True:
     print('alread connect')
 else:
@@ -24,27 +25,32 @@ else:
 #    time.sleep(3)
 
 #set time
-try:
-    ntptime.settime()
-except:
-    pass
 rtc = machine.RTC()
-# for time convert to second
-tampon1 = utime.time()
-# for gmt. For me gmt+3.
-# 1 hour = 3600 seconds
-# 3 hours = 10800 seconds
-tampon2 = tampon1 + 28800
-# for second to convert time
-(year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime(tampon2)
-# first 0 = week of year
-# second 0 = milisecond
-rtc.datetime((year, month, mday, 0, hour, minute, second, 0))
+rtc.ntp_sync(server="hr.pool.ntp.org", tz="CCT-8")
+rtc.synced()
+utime.gmtime()
+print("Time set:", utime.strftime("%c"))
+# try:
+#     ntptime.settime()
+# except:
+#     pass
+# rtc = machine.RTC()
+# # for time convert to second
+# tampon1 = utime.time()
+# # for gmt. For me gmt+3.
+# # 1 hour = 3600 seconds
+# # 3 hours = 10800 seconds
+# tampon2 = tampon1 + 28800
+# # for second to convert time
+# (year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime(tampon2)
+# # first 0 = week of year
+# # second 0 = milisecond
+# rtc.datetime((year, month, mday, 0, hour, minute, second, 0))
 
 
 
 #webrepl
-webrepl.start()
+#webrepl.start()
 
 
 #gc
--- main.py
+++ main.py
@@ -11,11 +11,17 @@ Date:	08/10/2017
 import display
 import time
 from machine import UART, Pin, random, TouchPad, GPS
+import ujson as json
+import urequests
 
+#onenet api配置
+api_url='http://api.heclouds.com'
+api_key='LAWZJ=Rs=NE2Ghj=lN==BgSQ7w0=' #请填入专属的api key
+api_headers={'api-key':api_key,'content-type': 'application/json'}
+device_id=46814719
 
 #define tft
 tft = display.TFT()
-
 tft.init(tft.ST7735R, speed=10000000, spihost=tft.HSPI, mosi=23, miso=19, clk=18, cs=26, dc=17, rst_pin=16,
          backl_pin=13, hastouch=False, bgr=True, width=128, height=128, backl_on=1, rot=tft.PORTRAIT_FLIP, splash=False)
 tft.font(tft.FONT_Ubuntu)
@@ -44,11 +50,24 @@ def show():
     tft.text(5, 40, longitude,tft.BLUE)
     tft.text(5, 60, altitude,tft.RED)
 
+def upload_onenet():
+    url=r'%s/devices/%s/datapoints?type=4' % (api_url,device_id)
+    strftime= "%04u-%02u-%02uT%02u:%02u:%02u" % time.localtime()[0:6]
+    print ("time:",strftime)
+    l = gps.getdata()
+    lon = str(l[1])
+    lat = str(l[2])
+    data={"lon":{strftime:lon},"lat":{strftime:lat}}
+    print (json.dumps(data))
+    res = urequests.post(url,headers=api_headers,data=json.dumps(data))
+    print ("status_code:",res.status_code)
+
 
 def main():
     while True:
         show()
-        time.sleep(5)
+        upload_onenet()
+        time.sleep(10)
 
 
 if __name__ == '__main__':
