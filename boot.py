eba5fbacee162ded7825622fd1df5a593fd20b52
Neo Lee <neo.lee.god@gmail.com>
2019-03-30 22:47:03 +0800

    test

--- boot.py
+++ boot.py
@@ -11,18 +11,21 @@ import machine
 import utime
 
 
-#set wlan
-#ap = network.WLAN(network.AP_IF)
-#ap.active(True)
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-#time.sleep(2)
-wlan.connect('XiaoPiDan', 'asdfzxcv123')
-time.sleep(3)
-if wlan.isconnected() == True:
-    print('alread connect')
-else:
-    wlan.connect('ZMI_E9C0', '63254450')
+set wlan
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+
+
+
+# wlan = network.WLAN(network.STA_IF)
+# wlan.active(True)
+# #time.sleep(2)
+# wlan.connect('XiaoPiDan', 'asdfzxcv123')
+# time.sleep(3)
+# if wlan.isconnected() == True:
+#     print('alread connect')
+# else:
+#     wlan.connect('ZMI_E9C0', '63254450')
 #    time.sleep(3)
 
 #set time
