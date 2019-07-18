1bc8d893cb8473da2f469619b74f4759997486c2
Neo Lee <neo.lee.god@gmail.com>
2019-07-18 23:11:47 +0800

    mp1.11

--- boot.py
+++ boot.py
@@ -24,28 +24,28 @@ else:
     wlan.connect('ZMI_E9C0', '63254450')
 #    time.sleep(3)
 
-#set time
-rtc = machine.RTC()
-rtc.ntp_sync(server="hr.pool.ntp.org", tz="CCT-8")
-rtc.synced()
-utime.gmtime()
-print("Time set:", utime.strftime("%c"))
+# #set time
+# rtc = machine.RTC()
+# rtc.ntp_sync(server="hr.pool.ntp.org", tz="CCT-8")
+# rtc.synced()
+# utime.gmtime()
+# print("Time set:", utime.strftime("%c"))
 # try:
 #     ntptime.settime()
 # except:
 #     pass
-# rtc = machine.RTC()
-# # for time convert to second
-# tampon1 = utime.time()
-# # for gmt. For me gmt+3.
-# # 1 hour = 3600 seconds
-# # 3 hours = 10800 seconds
-# tampon2 = tampon1 + 28800
-# # for second to convert time
-# (year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime(tampon2)
-# # first 0 = week of year
-# # second 0 = milisecond
-# rtc.datetime((year, month, mday, 0, hour, minute, second, 0))
+rtc = machine.RTC()
+# for time convert to second
+tampon1 = utime.time()
+# for gmt. For me gmt+3.
+# 1 hour = 3600 seconds
+# 3 hours = 10800 seconds
+tampon2 = tampon1 + 28800
+# for second to convert time
+(year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime(tampon2)
+# first 0 = week of year
+# second 0 = milisecond
+rtc.datetime((year, month, mday, 0, hour, minute, second, 0))
 
 
 
